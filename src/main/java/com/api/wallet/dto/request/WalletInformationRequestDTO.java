e0033c7696403b02ed3b3dfa098f1afcc37a5ff9
Diana Raquel Cruz Umanzor <dcruz@technisys.com>
2023-04-23 18:01:52 -0600

    Adding WalletInformationRequestDTO

---
+++ src/main/java/com/api/wallet/dto/request/WalletInformationRequestDTO.java
@@ -0,0 +1,20 @@
+package com.api.wallet.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WalletInformationRequestDTO {
+    private BigDecimal amount;
+    private Long userId;
+    private String accountNumber;
+}
